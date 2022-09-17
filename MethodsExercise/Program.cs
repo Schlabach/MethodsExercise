3c3c75470137bdda68ac9a7e367cd839f6b834a4
Schlabach <matthewshawn89@gmail.com>
2022-09-16 22:58:21 -0400

    initial commit

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,7 +4,29 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+                     Console.WriteLine("Hello, we are going to play a fun game of MadLibs, but I will need some input from you. First what is your name?");
+            string yourName = Console.ReadLine();
+
+            Console.WriteLine($"Hi {yourName}, what is your favorite color?");
+            string favoriteColor = Console.ReadLine();
+            Console.WriteLine($"Good choice, {favoriteColor} is a great color!");
+
+            Console.WriteLine("What is your favorite animal?");
+            string favoriteAnimal = Console.ReadLine();
+
+                if(favoriteAnimal == "penguin")
+                {
+                    Console.WriteLine("No way, that's mine too!");
+                }
+                else
+                {
+                    Console.WriteLine($"Penguins are better, but I guess {favoriteAnimal}s are ok.");
+                }
+
+            Console.WriteLine("The last thing I need to know is what is your favorite band?");
+            string favoriteBand = Console.ReadLine();
+
+            Console.WriteLine($"Alright {yourName}, this is a good one: The {favoriteAnimal} is the most impressive animal in the world. Not only can they fly, but they can camouflage due to being {favoriteColor}. Seeing one in real life is like being front row at a {favoriteBand} concert.");
         }
     }
 }

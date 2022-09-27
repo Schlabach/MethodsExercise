e598fc58108d30dae94985ce35248a0b37e9a3e1
Schlabach <matthewshawn89@gmail.com>
2022-09-27 06:57:01 -0400

    add second step

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -27,6 +27,34 @@
             string favoriteBand = Console.ReadLine();
 
             Console.WriteLine($"Alright {yourName}, this is a good one: The {favoriteAnimal} is the most impressive animal in the world. Not only can they fly, but they can camouflage due to being {favoriteColor}. Seeing one in real life is like being front row at a {favoriteBand} concert.");
+            
+            Console.WriteLine("Give me a number to add");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Give me a number to add to the first one");
+            int num2 = int.Parse(Console.ReadLine());
+
+            int sum = Sum(num1, num2);
+            Console.WriteLine($"The sum is: (sum)");
+
+            Console.WriteLine("Give me a number to multiply");
+            int x = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Give me a number to add to the first one");
+            int y = int.Parse(Console.ReadLine());
+
+            int product = Multiply(x, y);
+            Console.WriteLine($"The sum is: (product)");
         }
+        public static int Sum(int num1, int num2)
+            {
+                return num1 + num2;
+            }
+        
+        public static int Multiply(int x, int y)
+        {
+            return x * y;
+        }
+
     }
 }
